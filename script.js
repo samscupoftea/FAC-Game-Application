887640d5d1f6d6964e5f2431fccb80ce7a250737
sam hockaday <samuel.hockaday@gmail.com>
2023-07-05 20:07:17 +0100

    Updated JS canvas width and height

---
+++ script.js
@@ -0,0 +1,8 @@
+// Use query selector to target the html/DOM of <canvas> that I created within the body of my HTML page. 
+
+const canvas = document.querySelector('canvas');
+// Establish that the game space will be in the context of 2 dimensional space as oppose to 3d or something else. 
+const c = canvas.getBoundingClientRect('2d');
+
+canvas.width = 1024
+canvas.height = 576
